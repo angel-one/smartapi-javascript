730acca0430222574ca36d9d1ab07f26ef4942b0
Amol <amolbhagwat@gmail.com>
2023-10-18 10:19:03 +0530

    testcases added

--- example/test.js
+++ example/test.js
@@ -1,17 +1,17 @@
 let { SmartAPI, WebSocketClient, WebSocketV2 } = require('../lib');
 
 let smart_api = new SmartAPI({
-  api_key: "", // PROVIDE YOUR API KEY HERE
-  // OPTIONAL : If user has valid access token and refresh token then it can be directly passed to the constructor
-  access_token: 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6IkE1MjE2MzEzNCIsInJvbGVzIjowLCJ1c2VydHlwZSI6IlVTRVIiLCJ0b2tlbiI6ImV5SmhiR2NpT2lKSVV6VXhNaUlzSW5SNWNDSTZJa3BYVkNKOS5leUp6ZFdJaU9pSkJOVEl4TmpNeE16UWlMQ0psZUhBaU9qRTJPVGMxTXpFeE16QXNJbWxoZENJNk1UWTVOelF6TWpjMU5pd2lhblJwSWpvaU9Ea3pZMlUxWldVdFltRTNOQzAwT1RRd0xXRmhabUV0TnpJeVpHUmxPVFprWXpSbUlpd2liMjF1WlcxaGJtRm5aWEpwWkNJNk9Dd2ljMjkxY21ObGFXUWlPaUl6SWl3aWRYTmxjbDkwZVhCbElqb2lZMnhwWlc1MElpd2lkRzlyWlc1ZmRIbHdaU0k2SW5SeVlXUmxYMkZqWTJWemMxOTBiMnRsYmlJc0ltZHRYMmxrSWpvNExDSnpiM1Z5WTJVaU9pSXpJbjAuRzlxSnl1ZmxxVTdkM1JNckhGRzRkdXVaV3FJSHZPLTBmRkxpQmFCR0V1bm5SeDFsMWt0QktZWlpwczY2RzBNWVFwOXIxdWlWa0V6R05fZkctVWlJZXciLCJBUEktS0VZIjoiVkcyczM0Q3EiLCJpYXQiOjE2OTc0MzI4MTYsImV4cCI6MTY5NzUzMTEzMH0.cNBTyw40c0eW_ZyXi4Dh4ZZ5MVDykbWrsEXaBzercNl2JjNwD0hpU5I_7Y42uDsCP6StZX-MfMU6yNw5BSrNTg',
-  refresh_token: 'eyJhbGciOiJIUzUxMiJ9.eyJ0b2tlbiI6IlJFRlJFU0gtVE9LRU4iLCJSRUZSRVNILVRPS0VOIjoiZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnpkV0lpT2lKQk5USXhOak14TXpRaUxDSmxlSEFpT2pFMk9UYzBOell3TVRZc0ltbGhkQ0k2TVRZNU56UXpNamMxTml3aWFuUnBJam9pWVdKbU5tVmhNMll0TnprelpDMDBObU0xTFRneFlUVXRZMlUxT1Rjell6azVOV1V6SWl3aWRHOXJaVzRpT2lKU1JVWlNSVk5JTFZSUFMwVk9JaXdpZFhObGNsOTBlWEJsSWpvaVkyeHBaVzUwSWl3aWRHOXJaVzVmZEhsd1pTSTZJblJ5WVdSbFgzSmxabkpsYzJoZmRHOXJaVzRpZlEuTTRYb1dkTHVnS043YnJFSW1TZXMwc190X29DNTF1amdmc1NmbUFpOU5SRTVkYXNBUDVTUWhkejN1VHdEaGtyakVDWXZxTHFhTTBiMzBYN3BHMnNaX1EiLCJpYXQiOjE2OTc0MzI4MTZ9.wiFmh17e52K7YZbWaX4JD949RRDVd3C76gZ7C3RBpmygIN164jbpC0N1tHEWn_QLh4Fk8bQfAeSGRT1pTjxJag',
+	api_key: "VG2s34Cq", // PROVIDE YOUR API KEY HERE
+	// OPTIONAL : If user has valid access token and refresh token then it can be directly passed to the constructor
+	access_token: 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6IkE1MjE2MzEzNCIsInJvbGVzIjowLCJ1c2VydHlwZSI6IlVTRVIiLCJ0b2tlbiI6ImV5SmhiR2NpT2lKSVV6VXhNaUlzSW5SNWNDSTZJa3BYVkNKOS5leUp6ZFdJaU9pSkJOVEl4TmpNeE16UWlMQ0psZUhBaU9qRTJPVGMyTVRVMU1qUXNJbWxoZENJNk1UWTVOelV4TnpZNE5Dd2lhblJwSWpvaVpHTTVZVGc1Wm1FdE5UVXdOQzAwTm1KbExUaGhZMll0TkRObU5ERXlZVGsyT0dKbElpd2liMjF1WlcxaGJtRm5aWEpwWkNJNk9Dd2ljMjkxY21ObGFXUWlPaUl6SWl3aWRYTmxjbDkwZVhCbElqb2lZMnhwWlc1MElpd2lkRzlyWlc1ZmRIbHdaU0k2SW5SeVlXUmxYMkZqWTJWemMxOTBiMnRsYmlJc0ltZHRYMmxrSWpvNExDSnpiM1Z5WTJVaU9pSXpJbjAuS1VtazN5Y1MwRUpHdDgxb3FIZEFyNnRUVTE4Q0VYWHREQmJnWUJ4UDlWaWcySm9kX2dONmlIZ3NrMEFSY0JCN0JiaVJmSktqRjB1VnhKTnZFQnNFdlEiLCJBUEktS0VZIjoiVkcyczM0Q3EiLCJpYXQiOjE2OTc1MTc3NDQsImV4cCI6MTY5NzYxNTUyNH0.Ykl_ADBV9ocMmGDInt8BwJR41xK3vMuTZ4dHkHLpUt5Gvq53EGXZ5OUSN017xrNo2iAYYOfJm3yDZfOeh5Fiwg',
+	refresh_token: 'eyJhbGciOiJIUzUxMiJ9.eyJ0b2tlbiI6IlJFRlJFU0gtVE9LRU4iLCJSRUZSRVNILVRPS0VOIjoiZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnpkV0lpT2lKQk5USXhOak14TXpRaUxDSmxlSEFpT2pFMk9UYzFOakE1TkRRc0ltbGhkQ0k2TVRZNU56VXhOelk0TkN3aWFuUnBJam9pTVRRMU5qTmpNR1l0TUdSa01TMDBORE0wTFRrek9XTXRPRFl6TVRSa1ltTXpZalprSWl3aWRHOXJaVzRpT2lKU1JVWlNSVk5JTFZSUFMwVk9JaXdpZFhObGNsOTBlWEJsSWpvaVkyeHBaVzUwSWl3aWRHOXJaVzVmZEhsd1pTSTZJblJ5WVdSbFgzSmxabkpsYzJoZmRHOXJaVzRpZlEuNUk5X1I4RHV6NDd6bDE4X0FaOVpaS0VkLWxqRVVRWnEwU3hSSHVaWVB1OEdfbV9VbFd5em11WEdCSi1rSVhZZE1NOFRNWkltSzZKYng5WHZmSmNndVEiLCJpYXQiOjE2OTc1MTc3NDR9.nRzIcjFpKJ9j_xJhKEkzQw5-TFyVK4gwmtYvzoSt5q-5I8CBv8XebNMEDgwwVg25pE8QuIU3qWGfIw8HB70Jww',
 });
 
 // // If user does not have valid access token and refresh token then use generateSession method
 
 // }
 // smart_api
-// 	.generateSession('A52163134', '4321', '919073')
+// 	.generateSession('A52163134', '4321', '123295')
 // 	.then((data) => {
 // 		console.log(data);
 // 		return smart_api.getProfile();
@@ -60,23 +60,28 @@ let smart_api = new SmartAPI({
 
 // 		// 	// return smart_api.getOrderBook();
 
-				// smart_api.getOrderBook().then((data)=>{
-				// 	console.log(data);
-				// })
+// smart_api.getOrderBook().then((data)=>{
+// 	console.log(data);
+// })
 
-			// return smart_api.getTradeBook();
+// return smart_api.getTradeBook();
 
 // 		// 	// Portfolio Methods
-			smart_api
-        .getHolding()
-        .then((data) => {
-          console.log("Success: ", data);
-        })
-        .catch((err) => {
-          console.log("Error:", err);
-        });
+// 	smart_api
+// .getHolding()
+// .then((data) => {
+//   console.log("Success: ", data);
+// })
+// .catch((err) => {
+//   console.log("Error:", err);
+// });
 
-// 		// 	// return smart_api.getPosition();
+// smart_api.getPosition().then((data) => {
+// 	console.log("Success: ", data);
+// })
+// 	.catch((err) => {
+// 		console.log("Error:", err);
+// 	});
 
 // 		// 	// return smart_api.convertPosition({
 // 		// 	//     "exchange": "NSE",
@@ -89,74 +94,115 @@ let smart_api = new SmartAPI({
 // 		// 	// });
 
 // 		// 	// GTT Methods
-// 		// 	// return smart_api.createRule({
-// 		// 	//    "tradingsymbol" : "SBIN-EQ",
-// 		// 	//    "symboltoken" : "3045",
-// 		// 	//    "exchange" : "NSE",
-// 		// 	//    "producttype" : "MARGIN",
-// 		// 	//    "transactiontype" : "BUY",
-// 		// 	//    "price" : 100000,
-// 		// 	//    "qty" : 10,
-// 		// 	//    "disclosedqty": 10,
-// 		// 	//    "triggerprice" : 200000,
-// 		// 	//    "timeperiod" : 365
-// 		// 	// })
-// 		// 	// return smart_api.modifyRule({
-// 		// 	//             "id" : 1000014,
-// 		// 	//             "symboltoken" : "3045",
-// 		// 	//             "exchange" : "NSE",
-// 		// 	//             "qty" : 10
+	// smart_api
+    //     .createRule({
+    //       tradingsymbol: "SBIN-EQ",
+    //       symboltoken: "3045",
+    //       exchange: "NSE",
+    //       producttype: "MARGIN",
+    //       transactiontype: "BUY",
+    //       price: 100000,
+    //       qty: 10,
+    //       disclosedqty: 10,
+    //       triggerprice: 200000,
+    //       timeperiod: 365,
+    //     })
+    //     .then((data) => {
+    //       console.log("Success cancel: ", data);
+    //     })
+    //     .catch((err) => {
+    //       console.log("Error:", err);
+    //     });
+// smart_api
+//   .modifyRule({
+//     id: 1440101,
+//     symboltoken: "3045",
+//     exchange: "NSE",
+//     qty: 10,
+//   })
+//   .then((data) => {
+//     console.log("Success modify: ", data);
+//   })
+//   .catch((err) => {
+//     console.log("Error:", err);
+//   });
+// smart_api
+// 	.cancelRule({
+// 		id: 1440101,
+// 		symboltoken: "3045",
+// 		exchange: "NSE",
+// 	})
+// 	.then((data) => {
+// 		console.log("Success cancel: ", data);
+// 	})
+// 	.catch((err) => {
+// 		console.log("Error:", err);
+// 	});
+// smart_api
+//   .ruleDetails({
+//     id: 1440395,
+//   })
+//   .then((data) => {
+//     console.log("Success cancel: ", data);
+//   })
+//   .catch((err) => {
+//     console.log("Error:", err);
+//   });
+		// 	smart_api
+        // .ruleList({
+        //   status: ["NEW", "CANCELLED"],
+        //   page: 1,
+        //   count: 10,
+        // })
+        // .then((data) => {
+        //   console.log("Success cancel: ", data);
+        // })
+        // .catch((err) => {
+        //   console.log("Error:", err);
+        // });
 
-// 		// 	// })
-// 		// 	// return smart_api.cancelRule({
-// 		// 	//      "id" : 1000014,
-// 		// 	//      "symboltoken" : "3045",
-// 		// 	//      "exchange" : "NSE"
-// 		// 	// })
-// 		// 	// return smart_api.ruleDetails({
-// 		// 	//     "id" : 25
-// 		// 	// })
-// 		// 	// return smart_api.ruleList({
-// 		// 	//      "status" : ["NEW","CANCELLED"],
-// 		// 	//      "page" : 1,
-// 		// 	//      "count" : 10
-// 		// 	// })
-
-// 		// 	// Historical Methods
-// 		// 	// return smart_api.getCandleData({
-// 		// 	//     "exchange": "NSE",
-// 		// 	//     "symboltoken": "3045",
-// 		// 	//     "interval": "ONE_MINUTE",
-// 		// 	//     "fromdate": "2021-02-10 09:00",
-// 		// 	//     "todate": "2021-02-10 09:20"
-// 		// 	// })
+			//Historical Methods
+		// 	smart_api
+        // .getCandleData({
+        //   exchange: "NSE",
+        //   symboltoken: "3045",
+        //   interval: "ONE_MINUTE",
+        //   fromdate: "2021-02-10 09:00",
+        //   todate: "2021-02-10 09:20",
+        // })
+        // .then((data) => {
+        //   console.log("Success cancel: ", data);
+        // })
+        // .catch((err) => {
+        //   console.log("Error:", err);
+        // });
 
 
-		// Market Data Methods
-		// smart_api.marketData({
- 		// 			"mode": "FULL",
- 		// 			"exchangeTokens": {
- 		// 				"NSE": [
- 		// 					"3045"
- 		// 				]
- 		// 			}
-		// 		}).then((data) => {
-		// 			console.log(JSON.stringify(data, null, 2));
-		// 	        //  console.log(JSON.stringify(data))
-   		// 		});
+// Market Data Methods
+// smart_api.marketData({
+// 			"mode": "FULL",
+// 			"exchangeTokens": {
+// 				"NSE": [
+// 					"3045"
+// 				]
+// 			}
+// 		}).then((data) => {
+// 			console.log(JSON.stringify(data, null, 2));
+// 	        //  console.log(JSON.stringify(data))
+// 		});
 
-		// search Scrip Methods
-		// smart_api.searchScrip({
-		// 			"exchange": "BSE", 
-		// 			"searchscrip":"Titan"
-		// 		}).then((data)=>{
-		// 			console.log(data);
-		// 		})
+// search Scrip Methods
+// smart_api.searchScrip({
+// 			"exchange": "BSE",
+// 			"searchscrip":"Titan"
+// 		}).then((data)=>{
+// 			console.log(data);
+// 		})
 
-		// get all holding method
-		// smart_api.getAllHolding().then((data)=>{
-		// 	console.log(data);
-		// })
+// get all holding method
+// smart_api.getAllHolding().then((data)=>{
+// 	console.log(data);
+// })
 // })
 // .then((data) => {
 // 	console.log('PROFILE::', data);
@@ -252,8 +298,8 @@ let smart_api = new SmartAPI({
 // 		console.log('receiveTick:::::', data);
 // 	}
 
-	// setTimeout(() => {
-	// 	web_socket.close();
-	// }, 10000);
+// setTimeout(() => {
+// 	web_socket.close();
+// }, 10000);
 // });
 // ########################### Socket V2 Sample Code End Here ###########################
--- test/test.js
+++ test/test.js
@@ -6,9 +6,9 @@ let smart_api = new SmartAPI({
   api_key: "VG2s34Cq",
   // OPTIONAL : If user has valid access token and refresh token then it can be directly passed to the constructor
   access_token:
-    "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6IkE1MjE2MzEzNCIsInJvbGVzIjowLCJ1c2VydHlwZSI6IlVTRVIiLCJ0b2tlbiI6ImV5SmhiR2NpT2lKSVV6VXhNaUlzSW5SNWNDSTZJa3BYVkNKOS5leUp6ZFdJaU9pSkJOVEl4TmpNeE16UWlMQ0psZUhBaU9qRTJPVGMxTXpFeE16QXNJbWxoZENJNk1UWTVOelF6TWpjMU5pd2lhblJwSWpvaU9Ea3pZMlUxWldVdFltRTNOQzAwT1RRd0xXRmhabUV0TnpJeVpHUmxPVFprWXpSbUlpd2liMjF1WlcxaGJtRm5aWEpwWkNJNk9Dd2ljMjkxY21ObGFXUWlPaUl6SWl3aWRYTmxjbDkwZVhCbElqb2lZMnhwWlc1MElpd2lkRzlyWlc1ZmRIbHdaU0k2SW5SeVlXUmxYMkZqWTJWemMxOTBiMnRsYmlJc0ltZHRYMmxrSWpvNExDSnpiM1Z5WTJVaU9pSXpJbjAuRzlxSnl1ZmxxVTdkM1JNckhGRzRkdXVaV3FJSHZPLTBmRkxpQmFCR0V1bm5SeDFsMWt0QktZWlpwczY2RzBNWVFwOXIxdWlWa0V6R05fZkctVWlJZXciLCJBUEktS0VZIjoiVkcyczM0Q3EiLCJpYXQiOjE2OTc0MzI4MTYsImV4cCI6MTY5NzUzMTEzMH0.cNBTyw40c0eW_ZyXi4Dh4ZZ5MVDykbWrsEXaBzercNl2JjNwD0hpU5I_7Y42uDsCP6StZX-MfMU6yNw5BSrNTg",
+    "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6IkE1MjE2MzEzNCIsInJvbGVzIjowLCJ1c2VydHlwZSI6IlVTRVIiLCJ0b2tlbiI6ImV5SmhiR2NpT2lKSVV6VXhNaUlzSW5SNWNDSTZJa3BYVkNKOS5leUp6ZFdJaU9pSkJOVEl4TmpNeE16UWlMQ0psZUhBaU9qRTJPVGMyTVRVMU1qUXNJbWxoZENJNk1UWTVOelV4TnpZNE5Dd2lhblJwSWpvaVpHTTVZVGc1Wm1FdE5UVXdOQzAwTm1KbExUaGhZMll0TkRObU5ERXlZVGsyT0dKbElpd2liMjF1WlcxaGJtRm5aWEpwWkNJNk9Dd2ljMjkxY21ObGFXUWlPaUl6SWl3aWRYTmxjbDkwZVhCbElqb2lZMnhwWlc1MElpd2lkRzlyWlc1ZmRIbHdaU0k2SW5SeVlXUmxYMkZqWTJWemMxOTBiMnRsYmlJc0ltZHRYMmxrSWpvNExDSnpiM1Z5WTJVaU9pSXpJbjAuS1VtazN5Y1MwRUpHdDgxb3FIZEFyNnRUVTE4Q0VYWHREQmJnWUJ4UDlWaWcySm9kX2dONmlIZ3NrMEFSY0JCN0JiaVJmSktqRjB1VnhKTnZFQnNFdlEiLCJBUEktS0VZIjoiVkcyczM0Q3EiLCJpYXQiOjE2OTc1MTc3NDQsImV4cCI6MTY5NzYxNTUyNH0.Ykl_ADBV9ocMmGDInt8BwJR41xK3vMuTZ4dHkHLpUt5Gvq53EGXZ5OUSN017xrNo2iAYYOfJm3yDZfOeh5Fiwg",
   refresh_token:
-    "eyJhbGciOiJIUzUxMiJ9.eyJ0b2tlbiI6IlJFRlJFU0gtVE9LRU4iLCJSRUZSRVNILVRPS0VOIjoiZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnpkV0lpT2lKQk5USXhOak14TXpRaUxDSmxlSEFpT2pFMk9UYzBOell3TVRZc0ltbGhkQ0k2TVRZNU56UXpNamMxTml3aWFuUnBJam9pWVdKbU5tVmhNMll0TnprelpDMDBObU0xTFRneFlUVXRZMlUxT1Rjell6azVOV1V6SWl3aWRHOXJaVzRpT2lKU1JVWlNSVk5JTFZSUFMwVk9JaXdpZFhObGNsOTBlWEJsSWpvaVkyeHBaVzUwSWl3aWRHOXJaVzVmZEhsd1pTSTZJblJ5WVdSbFgzSmxabkpsYzJoZmRHOXJaVzRpZlEuTTRYb1dkTHVnS043YnJFSW1TZXMwc190X29DNTF1amdmc1NmbUFpOU5SRTVkYXNBUDVTUWhkejN1VHdEaGtyakVDWXZxTHFhTTBiMzBYN3BHMnNaX1EiLCJpYXQiOjE2OTc0MzI4MTZ9.wiFmh17e52K7YZbWaX4JD949RRDVd3C76gZ7C3RBpmygIN164jbpC0N1tHEWn_QLh4Fk8bQfAeSGRT1pTjxJag",
+    "eyJhbGciOiJIUzUxMiJ9.eyJ0b2tlbiI6IlJFRlJFU0gtVE9LRU4iLCJSRUZSRVNILVRPS0VOIjoiZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnpkV0lpT2lKQk5USXhOak14TXpRaUxDSmxlSEFpT2pFMk9UYzFOakE1TkRRc0ltbGhkQ0k2TVRZNU56VXhOelk0TkN3aWFuUnBJam9pTVRRMU5qTmpNR1l0TUdSa01TMDBORE0wTFRrek9XTXRPRFl6TVRSa1ltTXpZalprSWl3aWRHOXJaVzRpT2lKU1JVWlNSVk5JTFZSUFMwVk9JaXdpZFhObGNsOTBlWEJsSWpvaVkyeHBaVzUwSWl3aWRHOXJaVzVmZEhsd1pTSTZJblJ5WVdSbFgzSmxabkpsYzJoZmRHOXJaVzRpZlEuNUk5X1I4RHV6NDd6bDE4X0FaOVpaS0VkLWxqRVVRWnEwU3hSSHVaWVB1OEdfbV9VbFd5em11WEdCSi1rSVhZZE1NOFRNWkltSzZKYng5WHZmSmNndVEiLCJpYXQiOjE2OTc1MTc3NDR9.nRzIcjFpKJ9j_xJhKEkzQw5-TFyVK4gwmtYvzoSt5q-5I8CBv8XebNMEDgwwVg25pE8QuIU3qWGfIw8HB70Jww",
 });
 
 // generateSession api
@@ -61,6 +61,32 @@ describe("getProfile", function () {
   });
 
   it("Get Profile failed", function () {
+    if (res.success === false) {
+      expect(res).to.have.property("success", false);
+      expect(res).to.have.property("message");
+      expect(res).to.have.property("errorCode");
+      expect(res).to.have.property("data");
+    }
+  });
+});
+
+// getRMS 
+describe("getRMS", function () {
+  let res;
+  before(async function () {
+    res = await smart_api.getRMS();
+  });
+
+  it("Get RMS successful", function () {
+    if (res.status) {
+      expect(res).to.have.property("status", true);
+      expect(res).to.have.property("message", "SUCCESS");
+      expect(res).to.have.property("errorcode", "");
+      expect(res).to.have.property("data");
+    }
+  });
+
+  it("Get RMS failed", function () {
     if (res.success === false) {
       expect(res).to.have.property("success", false);
       expect(res).to.have.property("message");
@@ -256,4 +282,298 @@ describe("getHolding", function () {
   });
 });
 
+// getPosition api
+describe("getPosition", function () {
+  let res;
+  before(async function () {
+    res = await smart_api.getPosition();
+  });
+
+  it("Get position successful", function () {
+    if (res.status) {
+      expect(res).to.have.property("status", true);
+      expect(res).to.have.property("message", "SUCCESS");
+      expect(res).to.have.property("errorcode", "");
+      expect(res).to.have.property("data");
+    }
+  });
+
+  it("Get position failed", function () {
+    if (res.success === false) {
+      expect(res).to.have.property("success", false);
+      expect(res).to.have.property("message");
+      expect(res).to.have.property("errorCode");
+      expect(res).to.have.property("data");
+    }
+  });
+});
+
+// createRule api
+describe("createRule", function () {
+  let res;
+  before(async function () {
+    res = await smart_api.createRule({
+      tradingsymbol: "SBIN-EQ",
+      symboltoken: "3045",
+      exchange: "NSE",
+      producttype: "MARGIN",
+      transactiontype: "BUY",
+      price: 100000,
+      qty: 10,
+      disclosedqty: 10,
+      triggerprice: 200000,
+      timeperiod: 365,
+    });
+  });
+
+  it("Create rule successful", function () {
+    if (res.status) {
+      expect(res).to.have.property("status", true);
+      expect(res).to.have.property("message", "SUCCESS");
+      expect(res).to.have.property("errorcode", "");
+      expect(res).to.have.property("data");
+    }
+  });
+
+  it("Create rule failed", function () {
+    if (res.success === false) {
+      expect(res).to.have.property("success", false);
+      expect(res).to.have.property("message");
+      expect(res).to.have.property("errorCode");
+      expect(res).to.have.property("data");
+    }
+  });
+});
+
+// modifyRule api
+describe("modifyRule", function () {
+  let res;
+  before(async function () {
+    res = await smart_api.modifyRule({
+      id: 1440101,
+      symboltoken: "3045",
+      exchange: "NSE",
+      qty: 10,
+    });
+  });
+
+  it("Modify rule successful", function () {
+    if (res.status) {
+      expect(res).to.have.property("status", true);
+      expect(res).to.have.property("message", "SUCCESS");
+      expect(res).to.have.property("errorcode", "");
+      expect(res).to.have.property("data");
+    }
+  });
+
+  it("Modify rule failed", function () {
+    if (res.success === false) {
+      expect(res).to.have.property("success", false);
+      expect(res).to.have.property("message");
+      expect(res).to.have.property("errorCode");
+      expect(res).to.have.property("data");
+    }
+  });
+});
+
+// cancelRule api
+describe("cancelRule", function () {
+  let res;
+  before(async function () {
+    res = await smart_api.cancelRule({
+      id: 1440101,
+      symboltoken: "3045",
+      exchange: "NSE",
+    });
+  });
+
+  it("Cancel rule successful", function () {
+    if (res.status) {
+      expect(res).to.have.property("status", true);
+      expect(res).to.have.property("message", "SUCCESS");
+      expect(res).to.have.property("errorcode", "");
+      expect(res).to.have.property("data");
+    }
+  });
+
+  it("Cancel rule failed", function () {
+    if (res.success === false) {
+      expect(res).to.have.property("success", false);
+      expect(res).to.have.property("message");
+      expect(res).to.have.property("errorCode");
+      expect(res).to.have.property("data");
+    }
+  });
+});
+
+// ruleDetails api
+describe("ruleDetails", function () {
+  let res;
+  before(async function () {
+    res = await smart_api.ruleDetails({
+      id: 1440395,
+    });
+  });
+
+  it("Rule details successful", function () {
+    if (res.status) {
+      expect(res).to.have.property("status", true);
+      expect(res).to.have.property("message", "SUCCESS");
+      expect(res).to.have.property("errorcode", "");
+      expect(res).to.have.property("data");
+    }
+  });
+
+  it("Rule details failed", function () {
+    if (res.status === false) {
+      expect(res).to.have.property("status", false);
+      expect(res).to.have.property("message");
+      expect(res).to.have.property("errorcode");
+      expect(res).to.have.property("data");
+    }
+  });
+});
+
+// ruleList api
+describe("ruleList", function () {
+  let res;
+  before(async function () {
+    res = await smart_api.ruleList({
+      status: ["NEW", "CANCELLED"],
+      page: 1,
+      count: 10,
+    });
+  });
+
+  it("Rule list successful", function () {
+    if (res.status === true) {
+      expect(res).to.have.property("status", true);
+      expect(res).to.have.property("message", "SUCCESS");
+      expect(res).to.have.property("errorcode", "");
+      expect(res).to.have.property("data");
+    }
+  });
+
+  it("Rule list failed", function () {
+    if (res.status === 500) {
+      expect(res).to.have.property("status", 500);
+      expect(res).to.have.property("message");
+    }
+  });
+});
+
+// getCandleData api
+describe("getCandleData", function () {
+  let res;
+  before(async function () {
+    res = await smart_api.getCandleData({
+      exchange: "NSE",
+      symboltoken: "3045",
+      interval: "ONE_MINUTE",
+      fromdate: "2021-02-10 09:00",
+      todate: "2021-02-10 09:20",
+    });
+  });
+
+  it("Get candle data successful", function () {
+    if (res.status === true) {
+      expect(res).to.have.property("status", true);
+      expect(res).to.have.property("message", "SUCCESS");
+      expect(res).to.have.property("errorcode", "");
+      expect(res).to.have.property("data");
+    }
+  });
+
+  it("Get candle data failed", function () {
+    if (res.status === false) {
+      expect(res).to.have.property("status", false);
+      expect(res).to.have.property("message");
+      expect(res).to.have.property("errorcode");
+      expect(res).to.have.property("data");
+    }
+  });
+});
+
+// marketData api
+describe("marketData", function () {
+  let res;
+  before(async function () {
+    res = await smart_api.marketData({
+      mode: "FULL",
+      exchangeTokens: {
+        NSE: ["3045"],
+      },
+    });
+  });
+
+  it("Market data successful", function () {
+    if (res.status === true) {
+      expect(res).to.have.property("status", true);
+      expect(res).to.have.property("message", "SUCCESS");
+      expect(res).to.have.property("errorcode", "");
+      expect(res).to.have.property("data");
+    }
+  });
+
+  it("Market data failed", function () {
+    if (res.status === false) {
+      expect(res).to.have.property("status", false);
+      expect(res).to.have.property("message");
+      expect(res).to.have.property("errorcode");
+      expect(res).to.have.property("data");
+    }
+  });
+});
+
+// searchScrip api
+describe("searchScrip", function () {
+  let res;
+  before(async function () {
+    res = await smart_api.searchScrip({
+      exchange: "BSE",
+      searchscrip: "Titan",
+    });
+  });
+
+  it("Search scrip successful", function () {
+    if (res) {
+      expect(res)
+    }
+  });
+
+  it("Search scrip failed", function () {
+    if (res.status === false) {
+      expect(res).to.have.property("status", false);
+      expect(res).to.have.property("message");
+      expect(res).to.have.property("errorcode");
+      expect(res).to.have.property("data");
+    }
+  });
+});
+
+// getAllHolding api
+describe("getAllHolding", function () {
+  let res;
+  before(async function () {
+    res = await smart_api.getAllHolding();
+  });
+
+  it("Get all holding successful", function () {
+    if (res.status === true) {
+      expect(res).to.have.property("status", true);
+      expect(res).to.have.property("message", "SUCCESS");
+      expect(res).to.have.property("errorcode", "");
+      expect(res).to.have.property("data");
+    }
+  });
+
+  it("Get all holding failed", function () {
+    if (res.success === false) {
+      expect(res).to.have.property("success", false);
+      expect(res).to.have.property("message");
+      expect(res).to.have.property("errorCode");
+      expect(res).to.have.property("data");
+    }
+  });
+});
 
